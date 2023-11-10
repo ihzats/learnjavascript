6faf6ece5e349e7f9eb219c1587d4b9294f79005
unknown <robbyihza@gmail.com>
2023-11-10 22:35:45 +0700

    assigment operator

---
+++ assignment_operator.js
@@ -0,0 +1,48 @@
+// Assignment =
+let nama = "Budi";
+let umur = 20;
+let apakahSudahMenikah = false;
+let jumlahAnak;
+
+console.log(nama); // Output: Budi
+console.log(umur); // Output: 20
+console.log(apakahSudahMenikah); // Output: false
+console.log(jumlahAnak); // Output: undefined
+
+// Addition Assignment +=
+// let bilangan = 10;
+
+// bilangan += 5;
+
+// console.log(bilangan); // Output: 15
+
+// Subtraction Assignment -=
+// let bilangan = 10;
+
+// bilangan -= 5;
+
+// console.log(bilangan); // Output: 5
+
+// Multiplication Assignment *=
+let bilangan = 10;
+
+bilangan *= 5;
+
+console.log(bilangan); // Output: 50
+
+// Division Assignment /=
+
+// let bilangan = 10;
+// bilangan /= 5;
+// console.log(bilangan); // Output: 2
+
+// Modulus Assignment %=
+
+// let bilangan = 10;
+// bilangan %= 5;
+// console.log(bilangan); // Output: 0
+
+// Exponent Assignment **=
+// let bilangan = 10;
+// bilangan **= 5;
+// console.log(bilangan); // Output: 100000
