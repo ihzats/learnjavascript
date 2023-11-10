87c6fb3f00e65db0d9e3b3cd61bcc01b32cd67c1
unknown <robbyihza@gmail.com>
2023-11-10 22:40:49 +0700

    string_operator

---
+++ string_operator.js
@@ -0,0 +1,18 @@
+// operator +
+let namaDepan = "Albert";
+let namaBelakang = "Einstein";
+
+console.log(namaDepan + " " + namaBelakang); // Output: Albert Einstein
+
+// operator +=
+let kata = "Halo ";
+kata += "Dunia";
+
+console.log(kata); // Output: Halo Dunia
+
+
+// contoh string dan number
+let kata2 = "Kambing ada ";
+let bilangan = 5;
+
+console.log(kata2 + bilangan); // Output: Kambing ada 5
