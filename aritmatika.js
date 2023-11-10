3329340d7ea0807b61ce00dae0a22ae9cd739dd1
unknown <robbyihza@gmail.com>
2023-11-10 22:28:16 +0700

    first commit

---
+++ aritmatika.js
@@ -0,0 +1,33 @@
+// var bil1 = 10;
+// var bil2 = 3;
+
+// //Penjumlahan
+// console.log(bil1 + bil2);
+
+// Pengurangan
+// console.log(bil1 - bil2);
+
+// Perkalian
+// console.log(bil1*bil2);
+
+// Pembagian
+// console.log(bil1 / bil2)
+
+// Pangkat
+// console.log(bil1**bil2)
+
+// Modulus
+// console.log(bil1%bil2)
+
+// Increment
+// var bilangan=1001;
+// bilangan++;
+
+// console.log(bilangan);
+
+
+// Decement
+
+var bilangan =100;
+bilangan--;
+console,log(bilangan)
