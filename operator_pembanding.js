ad58d5fd4557872153b1e621b79e85c59a907c7b
unknown <robbyihza@gmail.com>
2023-11-10 22:44:17 +0700

    operator_pembanding

---
+++ operator_pembanding.js
@@ -0,0 +1,6 @@
+// Operator == (Loose Equality)
+let bilangan = 10;
+
+console.log(bilangan == 10); // Output: true
+console.log(bilangan == 8); // Output: false
+console.log(bilangan == "10"); // Output: true
